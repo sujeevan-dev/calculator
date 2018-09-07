d2b7563b83a578da144c21e7ecd7491e895d1ffa
sujeevan <psujeevan@gmail.com>
2018-09-07 21:06:28 +0500

    First commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/sujeevan81/pen/ZyXxOv.
+
+ This is my first advance front-end developer certification course in freeCodeCamp. The calculator is built with HTML, CSS and JavaScript
---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+	var buttons = document.getElementById('buttons');
+	var clear = document.getElementById('clear');
+	var answer = document.getElementById('answer');
+
+	// clicking on each button
+	buttons.addEventListener('click', function (e) {
+
+		if (e.target.nodeName === 'LI') {
+
+			// get value of buttons
+			var v = e.target.innerHTML;
+
+			if (v === '=') {
+				try {
+					answer.innerHTML = eval(answer.innerHTML);
+				} catch(e) {
+					answer.innerHTML = e.message;
+				}
+			} else {
+				answer.innerHTML += v;
+			}
+		}
+	});
+
+	// clear the field
+	clear.addEventListener('click', function () {
+		answer.innerHTML = '';
+	});
+};
